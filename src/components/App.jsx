6812282e75c38e9f287f31f5649c92af69a5c268
Aleksey Zgeria <63537531+yukehi@users.noreply.github.com>
2021-01-03 14:16:55 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name="Beyonce"
+        imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        phone="+123 456 789"
+        email="b@beyonce.com"
+      />
+      <Card
+        name="Jack Bauer"
+        imgURL="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        phone="+987 654 321"
+        email="jack@nowhere.com"
+      />
+      <Card
+        name="Chuck Norris"
+        imgURL="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        phone="+918 372 574"
+        email="gmail@chucknorris.com"
+      />
+    </div>
+  );
+}
+
+export default App;
